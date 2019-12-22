9fa1df8eb317b93bc5db08d1ecb9fee831e9ef85
Santosh Pawan Kumar Bodapati <santoshbodapati@gmail.com>
2019-12-22 15:29:08 +0530

    Committing after adding other stuff to the fileparser.c
    Didn't get compiled yet.

--- XmlBasedCalculator/learnings/fileparse.c
+++ XmlBasedCalculator/learnings/fileparse.c
@@ -2,12 +2,29 @@
 /* This is for file parsing and how do we do it.    */
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-void int main() {
+int main() {
 	FILE* fp;
 	char file_name[100],c;
 
 	printf("Enter the filename: ");
-	scanf("%s",&file_name);
+	scanf("%s",file_name);
 
+	fp = fopen(file_name,'r');
+
+	if (!fp) {
+		printf("Nothing in file\n");
+		printf("something went wrong: %s", strerror(errno));
+		exit(1);
+	}
+	c = fgetc(fp);
+
+	while (c!=EOF) {
+		printf ("%c",c);
+		c = fgetc(fp);
+	}
+	fclose (fp);
+	return 0;
 }
