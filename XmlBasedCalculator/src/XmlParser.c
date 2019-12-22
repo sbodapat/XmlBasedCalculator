9aee7e4a79d8677ed27cbceac8ad8de436ab824d
Santosh Pawan Kumar Bodapati <santoshbodapati@gmail.com>
2019-12-22 13:58:55 +0530

    changed the function call in xmlparser.c

--- XmlBasedCalculator/src/XmlParser.c
+++ XmlBasedCalculator/src/XmlParser.c
@@ -1,6 +1,8 @@
+/* This file is meant to fetch the xml file; parse and give it to the other files for processing */
 #include <stdio.h>
+#include <string.h>
 
-int main () {
-  
-
+int xmlparser (char* fp) {
+    /*xmlparser function gets the filepointer as an input from the Config Parser file.*/
+    
 }
