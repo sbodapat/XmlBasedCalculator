5074bbd80bd1a0d3d502358214bb204509bf311c
Santosh Pawan Kumar Bodapati <santoshbodapati@gmail.com>
2019-12-22 17:04:28 +0530

    Committing after adding the option to count the number of counts. Some bug in the code. The count is only happeing once.

--- XmlBasedCalculator/learnings/fileparse.c
+++ XmlBasedCalculator/learnings/fileparse.c
@@ -4,16 +4,26 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+int compareString (char* tempStorage);
+
 
 int main() {
 	FILE* fp;
 	char file_name[100];
 	char c;
+	char tempStorage[10];
+	int whiteSpaceCounter,carriageReturnCounter,i,numberOfCounts,countWordCount;
+	/* Initializing all the variables */
+	whiteSpaceCounter = 0;
+	carriageReturnCounter = 0;
+	i=0;
+	numberOfCounts = 0;
+	countWordCount = 0;
 
 	printf("Enter the filename: ");
-	scanf("%s",file_name);
+	/*scanf("%s",file_name);*/
 
-	fp = fopen(file_name,"r"); /* Needs to have double quote to work */
+	fp = fopen("a.txt","r"); /* Needs to have double quote to work */
 
 	if (!fp) {
 		printf("Nothing in file\n");
@@ -23,9 +33,48 @@ int main() {
 	c = fgetc(fp);
 
 	while (c!=EOF) {
-		printf ("%c",c);
+		/*printf ("%c",c);*/
+		tempStorage[i] = c;
+		i++;
+		/* This is to count white spaces in the text file */
+		if (c == 32U) {
+			whiteSpaceCounter++;
+		}
+		/* It ends here */
+		if (c == 13U) {
+			carriageReturnCounter++;
+		}
+		if (c == 32U || c == 13U || c == 46U) {
+		    printf("\n From Code: Am I entering here\n");
+			numberOfCounts = compareString (&tempStorage);
+			if (numberOfCounts){
+				countWordCount++;
+			}
+			i = 0;
+		}
 		c = fgetc(fp);
 	}
 	fclose (fp);
+	printf("\nWhitespaces are: %d\n",whiteSpaceCounter);
+	printf("Carriage Return are: %d\n",carriageReturnCounter);
+	printf("number of count in the text file are: %d\n",countWordCount);
 	return 0;
+}
+
+int compareString (char* tempStorage) {
+    int i;
+	for (i=0;i<10;i++) {
+		printf("%c",tempStorage[i]);
+		printf(" index:%d ",i);
+	}
+	if (tempStorage[0] == 'c' && tempStorage[1] == 'o' && tempStorage[2] == 'u'
+	    && tempStorage[3] == 'n' && tempStorage[4] == 't' && (tempStorage[5] == ' ' || 
+		tempStorage[5] == 13U || tempStorage[5] == '.')){
+			printf ("\nFrom code: Entering the true part of the function\n");
+			
+			return 1;
+		}
+	else {
+		return 0;
+	}
 }
